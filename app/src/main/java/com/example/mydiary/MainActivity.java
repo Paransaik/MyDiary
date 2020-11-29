900ec7360aa596d110c643bdb92c9ae57f044a25
Paransaik <jty419@naver.com>
2020-11-30 02:20:11 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mydiary/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.mydiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/*
+* Department of Cybersecurity
+* Smart Device Programming
+* Made by Taeyeong, Jeong
+* github: Paransaik
+* url: https://github.com/Paransaik
+* date: `20.11.28.~xx.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
